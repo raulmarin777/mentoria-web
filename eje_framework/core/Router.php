933d999f9c9332f731c725363c6d27acca15b2c5
Raul Marin <raul.marin@segic.usach.cl>
2021-05-11 14:38:25 -0400

    se agrega Request.php

--- eje_framework/core/Router.php
+++ eje_framework/core/Router.php
@@ -3,15 +3,25 @@
 namespace app\core;
 
 class Router{
+    Public Request $request;
     protected array $routes = [];
+
+    public function __construct (\app\core\Request $request){
+        $this->request = $request;
+    }
 
     public function get($path, $callback){
         $this->routes['get'][$path] = $callback;
     }
 
     public function resolve(){
-        echo '<pre>';
+        /*echo '<pre>';
         var_dump($_SERVER);
-        echo '</pre>';
+        echo '</pre>';*/
+        $path = $this->request->getPath();
+        $method = $this->request->getMethod();
+
+        var_dump($path);
+        var_dump($method);
     }
 }
