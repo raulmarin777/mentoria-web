4b1db94a08fa2b3f4dabc791d48cba2cbab521f3
Raul Marin <raul.marin@segic.usach.cl>
2021-05-11 15:02:09 -0400

    se agrega Request.php

--- eje_framework/core/Router.php
+++ eje_framework/core/Router.php
@@ -22,7 +22,12 @@ class Router{
         $path = $this->request->getPath();
         $method = $this->request->getMethod();
 
-        $callback = $this->routes[$method][$path];
+        $callback = $this->routes[$method][$path] ?? false;
+
+        if($callback == false){
+            echo "Not Found";
+            exit;
+        }
         /*print_r($this->routes);
         var_dump($path);
         var_dump($method);*/
