8e121eef812aaad5c4a86679eac2bf1416885390
Raul Marin <raul.marin@segic.usach.cl>
2021-05-20 16:12:57 -0400

    se implementa ROOT_DIR

--- eje_framework/core/Router.php
+++ eje_framework/core/Router.php
@@ -40,12 +40,20 @@ class Router{
     public function renderView($view){
         //interpolacion
         $layoutContent = $this->layoutContent();
-        include_once Application::$ROOT_DIR . "/views/$view.php";
+        $viewContent = $this->renderOnlyView();
+        return str_replace('{{content}}', $viewContent, $layoutContent);
     }
 
     public function layoutContent(){
         ob_start();
         include_once Application::$ROOT_DIR . "/views/layouts/main.php";
+        return ob_get_clean();
+    }
+
+    public function renderOnlyView($view){
+        ob_start();
+        $layoutContent = $this->layoutContent();
+        include_once Application::$ROOT_DIR . "/views/$view.php";
         return ob_get_clean();
     }
 }
