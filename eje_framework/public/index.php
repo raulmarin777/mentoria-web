e315a11f7e209094688d6e01776d97b0f724729b
Raul Marin <raul.marin@segic.usach.cl>
2021-05-20 15:56:06 -0400

    se implementa ROOT_DIR

--- eje_framework/public/index.php
+++ eje_framework/public/index.php
@@ -4,10 +4,7 @@ require_once __DIR__ . "/../vendor/autoload.php";
 
 use app\core\Application;
 
-/var/www/systems/mentoria-web/eje_framework/public
-/var/www/systems/mentoria-web/eje_framework
  /*  echo __DIR__;         /var/www/systems/mentoria-web/eje_framework/public */
-echo "<br>";
 /* echo dirname(__DIR__);  /var/www/systems/mentoria-web/eje_framework */
 
 $app = new Application(dirname(__DIR__));
