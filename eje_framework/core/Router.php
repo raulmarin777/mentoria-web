4a38d30cbcae6ff7833716bc8883abd27239cb90
Raul Marin <raul.marin@segic.usach.cl>
2021-05-27 11:28:54 -0400

    se implementa controlador en router

--- eje_framework/core/Router.php
+++ eje_framework/core/Router.php
@@ -2,83 +2,90 @@
 
 namespace app\core;
 
-class Router{
-
+class Router
+{
     public Request $request;
     public Response $response;
 
-    protected array $routes = [];
+    protected array $routes = [];    
 
-    public function __construct (Request $request, Response $response){
-        $this->request = $request;
-        $this->response = $response;
+    public function __construct(Request $request, Response $response)
+    {
+       $this->request = $request;
+       $this->response = $response;
     }
-
-    public function get($path, $callback){
+    
+    public function get($path, $callback)
+    {
         $this->routes['get'][$path] = $callback;
     }
 
-    public function post($path, $callback){
+    public function post($path, $callback)
+    {
         $this->routes['post'][$path] = $callback;
     }
 
-    public function resolve(){
+    public function resolve()
+    {
         /*echo '<pre>';
         var_dump($_SERVER);
-        echo '</pre>';*/
+        echo '</pre>';
+        exit;*/
+
         $path = $this->request->getPath();
         $method = $this->request->getMethod();
 
         $callback = $this->routes[$method][$path] ?? false;
-        //Application::$app;
-        /*var_dump($path);
-        var_dump($method);*/
 
-        if($callback == false){
-            //Application::$app->response->setStatusCode(404); es lo mismo q lo de abajo
+        if ($callback === false){
+            //Application::$app->response->setStatusCode(404);
             $this->response->setStatusCode(404);
             //return $this->renderContent("Not Found");
-            return $this->renderView("_404");
+            return $this->renderView("_404");            
         }
-        /*print_r($this->routes);*/
 
-        if (is_string($callback)){
+        if (is_string($callback)) {
             return $this->renderView($callback);
         }
         
-        /*$callback = array(2) { 
-            [0]=> string(30) "app\Controllers\SiteController" 
-            [1]=> string(4) "home" }*/
-
+        /*array(2) { 
+            [0]=> string(30) "app\controllers\SiteController" 
+            [1]=> string(4) "home" 
+        } */
         if (is_array($callback)) {
-            $callback[0] = new $callback[0]();
-            //esto transforma el string a objeto            
+            $callback[0] = new $callback[0]();            
         }
 
         return call_user_func($callback);
+
     }
 
-    public function renderContent($viewContent){
+    public function renderContent($viewContent)
+    {
         $layoutContent = $this->layoutContent();
         return str_replace('{{content}}', $viewContent, $layoutContent);
-    }    
+    }
 
-    public function renderView($view){
-        //interpolacion
+    public function renderView($view)
+    {
         $layoutContent = $this->layoutContent();
         $viewContent = $this->renderOnlyView($view);
+
         return str_replace('{{content}}', $viewContent, $layoutContent);
+
+        //include_once Application::$ROOT_DIR . "/views/$view.php";
     }
 
-    public function layoutContent(){
+    public function layoutContent()
+    {
         ob_start();
         include_once Application::$ROOT_DIR . "/views/layouts/main.php";
         return ob_get_clean();
     }
 
-    public function renderOnlyView($view){
+    public function renderOnlyView($view)
+    {
         ob_start();
-//        $layoutContent = $this->layoutContent();
         include_once Application::$ROOT_DIR . "/views/$view.php";
         return ob_get_clean();
     }
