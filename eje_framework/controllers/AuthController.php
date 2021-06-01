4ff1d443a242d6a13a3b6f91f28a444af32ee622
Raul Marin <raul.marin@segic.usach.cl>
2021-06-01 10:20:09 -0400

    Se añaden login y register

--- eje_framework/controllers/AuthController.php
+++ eje_framework/controllers/AuthController.php
@@ -7,10 +7,10 @@ use app\core\Request;
 
 class AuthController extends Controller{
     public function login(){
-        $this->render('login');
+        return $this->render('login');
     }
 
     public function register(Request $request){
-        $this->render('register');
+        return $this->render('register');
     }
 }
