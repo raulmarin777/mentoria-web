a1d1a8f7827baed73f00c6305de9a0821fb868f3
Raul Marin <raul.marin@segic.usach.cl>
2021-06-10 10:12:59 -0400

    se implementan nuevas validaciones

--- eje_framework/views/register.php
+++ eje_framework/views/register.php
@@ -11,7 +11,7 @@
   </div>
   <div class="mb-3">
     <label class="form-label">Email</label>
-    <input type="email" name="email" class="form-control">
+    <input type="text" name="email" class="form-control">
   </div>
   <div class="mb-3">
     <label class="form-label">Password</label>
