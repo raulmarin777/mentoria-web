85bee2bccf4327f4f0ba9c8d7e15554a9dcfae6c
Raul Marin <raul.marin@segic.usach.cl>
2021-06-11 11:54:36 -0400

    se implementan widgets

--- eje_framework/views/register.php
+++ eje_framework/views/register.php
@@ -3,34 +3,15 @@
 <?php $form = \app\core\widgets\Form::begin('', 'POST') ?>
 <div class="row">
 <div class="col">
-<?php $form->field($model, 'firstName') ?>
+<?= $form->field($model, 'firstName') ?>
 </div>
 <div class="col">
-<?php $form->field($model, 'lastName') ?>
+<?= $form->field($model, 'lastName') ?>
 </div>
 </div>
-<?php $form->field($model, 'email')->mailField() ?>
-<?php $form->field($model, 'password')->passwordField() ?>
-<?php $form->field($model, 'confirmPassword') ?>
+<?= $form->field($model, 'email')->mailField() ?>
+<?= $form->field($model, 'password')->passwordField() ?>
+<?= $form->field($model, 'confirmPassword') ?>
 <button type="submit" class="btn btn-primary">Save</button>
 <?= \app\core\widgets\Form::end() ?>
 
-<h1>Register !!!</h1>
-
-<?php $form = \app\core\widgets\Form::begin('','POST') ?>
-<div class="row">
-<div class="col">
-<?= $form->field($model,'firstname') ?>
-</div>
-<div class="col">
-<?= $form->field($model,'lastname') ?>
-</div>
-</div>
-
-
-<?= $form->field($model,'email')->mailField() ?>
-<?= $form->field($model,'password')->passwordField() ?>
-<?= $form->field($model,'confirmPassword')->passwordField() ?>
-
-<button type="submit" class="btn btn-primary">Save</button>
-<?php \app\core\widgets\Form::end() ?>
