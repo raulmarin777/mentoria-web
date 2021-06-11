a8566a5ae0cf04cd6e0b8c9992abf6c6da7532ea
Raul Marin <raul.marin@segic.usach.cl>
2021-06-11 11:45:20 -0400

    se implementan widgets

--- eje_framework/views/register.php
+++ eje_framework/views/register.php
@@ -1,11 +1,17 @@
 <h1>Register !!!</h1>
 
 <?php $form = \app\core\widgets\Form::begin('', 'POST') ?>
-
+<div class="row">
+<div class="col">
 <?php $form->field($model, 'firstName') ?>
+</div>
+<div class="col">
 <?php $form->field($model, 'lastName') ?>
+</div>
+</div>
 <?php $form->field($model, 'email')->mailField() ?>
 <?php $form->field($model, 'password')->passwordField() ?>
 <?php $form->field($model, 'confirmPassword') ?>
 <button type="submit" class="btn btn-primary">Save</button>
 <?= \app\core\widgets\Form::end() ?>
+
