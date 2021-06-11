d89e80df5723ad35b8caf0eeee62a00d69b19136
Raul Marin <raul.marin@segic.usach.cl>
2021-06-11 11:47:24 -0400

    se implementan widgets

--- eje_framework/core/widgets/Form.php
+++ eje_framework/core/widgets/Form.php
@@ -1,20 +1,25 @@
-<?php
 
+
+<?php
 namespace app\core\widgets;
 
 use app\core\Model;
 
-class Form{
-    public static function begin($action, $method){
-        //return sprintf('<form action="%s" method="%s">', $action, $method);
-        echo sprintf('<form action="%s" method="%s">', $action, $method);
-        return new Form();
+class Form
+{
+    public static function begin($action,$method)
+    {
+        //format python
+       // return sprintf('<form action="%s" method="%s">',$action,$method);
+       echo sprintf('<form action="%s" method="%s">',$action,$method);
+       return new Form();
     }
-    public static function end(){
-        echo '</form>';
+    public static function end()
+    {
+        echo '<form>';
     }
-    //Polimorfismo
-    public function field(Model $model, string $attribute){
-        return new Field($model, $attribute);
+    public function field(Model $model,string $attribute){
+       return new Field($model,  $attribute); 
     }
+
 }
--- eje_framework/views/register.php
+++ eje_framework/views/register.php
@@ -1,14 +1,29 @@
+<h1>Register !!!</h1>
 
+<?php $form = \app\core\widgets\Form::begin('', 'POST') ?>
+<div class="row">
+<div class="col">
+<?php $form->field($model, 'firstName') ?>
+</div>
+<div class="col">
+<?php $form->field($model, 'lastName') ?>
+</div>
+</div>
+<?php $form->field($model, 'email')->mailField() ?>
+<?php $form->field($model, 'password')->passwordField() ?>
+<?php $form->field($model, 'confirmPassword') ?>
+<button type="submit" class="btn btn-primary">Save</button>
+<?= \app\core\widgets\Form::end() ?>
 
 <h1>Register !!!</h1>
 
 <?php $form = \app\core\widgets\Form::begin('','POST') ?>
 <div class="row">
 <div class="col">
-<?= $form->field($model,'firstName') ?>
+<?= $form->field($model,'firstname') ?>
 </div>
 <div class="col">
-<?= $form->field($model,'lastName') ?>
+<?= $form->field($model,'lastname') ?>
 </div>
 </div>
 
