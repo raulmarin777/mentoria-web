100401032c76b41d883a928a208aca458a667c83
Raul Marin <raul.marin@segic.usach.cl>
2021-06-11 11:50:45 -0400

    se implementan widgets

--- eje_framework/core/widgets/Form.php
+++ eje_framework/core/widgets/Form.php
@@ -1,25 +1,20 @@
-
-
 <?php
+
 namespace app\core\widgets;
 
 use app\core\Model;
 
-class Form
-{
-    public static function begin($action,$method)
-    {
-        //format python
-       // return sprintf('<form action="%s" method="%s">',$action,$method);
-       echo sprintf('<form action="%s" method="%s">',$action,$method);
-       return new Form();
+class Form{
+    public static function begin($action, $method){
+        //return sprintf('<form action="%s" method="%s">', $action, $method);
+        echo sprintf('<form action="%s" method="%s">', $action, $method);
+        return new Form();
     }
-    public static function end()
-    {
-        echo '<form>';
+    public static function end(){
+        echo '</form>';
     }
-    public function field(Model $model,string $attribute){
-       return new Field($model,  $attribute); 
+    //Polimorfismo
+    public function field(Model $model, string $attribute){
+        return new Field($model, $attribute);
     }
-
 }
