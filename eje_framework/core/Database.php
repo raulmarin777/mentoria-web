3057c6be97ad3261a0486ff2714475689d3ae953
Raul Marin <raul.marin@segic.usach.cl>
2021-06-17 15:18:48 -0400

    Se  genera creacion de tabla

--- eje_framework/core/Database.php
+++ eje_framework/core/Database.php
@@ -21,6 +21,19 @@ class Database{
     }
 
     public function applyMigrations(){
-        echo "Running applyMigrations ";
+        $this->createMigrationsTable();
+        $appliendMigrations = $this->getAppliedMigrations();
+    }
+
+    public function createMigrationsTable(){
+        $this->pdo->exec("CREATE TABLE IF NOT EXISTS `migrations` ( `id` INT NOT NULL AUTO_INCREMENT , `migration` VARCHAR(255) NOT NULL , `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`)) ENGINE = InnoDB; ");
+    }
+
+    public function getAppliedMigrations(){
+        $sql = "SELECT migration FROM migrations";
+        $statement = $this->pdo->prepare($sql);
+        $statement->execute();
+
+        return $statement->fetchAll(\PDO::FETCH_COLUMN);
     }
 }
