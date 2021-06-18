b04fde701e79bd900a5f472eb5288d0e729bbc91
Raul Marin <raul.marin@segic.usach.cl>
2021-06-18 10:32:06 -0400

    se implementa creacion de tabla user

--- eje_framework/migrations/m00001_initial.php
+++ eje_framework/migrations/m00001_initial.php
@@ -1,31 +1,35 @@
 <?php
 
-class m00001_initial{
+class m0001_initial
+{
     public function up()
-    {
-        $db= \app\core\Application::$app->db;
+    {        
+        $db = \app\core\Application::$app->db;
+
         $sql = "CREATE TABLE `users` (
-                                      `id` int NOT NULL,
-                               `full_name` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-                                   `email` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-                               `user_name` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-                                `password` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL
-                                      ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci";
+            `id` int NOT NULL,
+            `full_name` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+            `email` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+            `user_name` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+            `password` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL
+          ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci";
+          
+          $db->pdo->exec($sql);
 
-        $db->pdo-exec($sql);
-        $sql = "ALTER TABLE `users`
-                  ADD PRIMARY KEY (`id`)";
+          $sql = "ALTER TABLE `users`
+            ADD PRIMARY KEY (`id`)";
+          $db->pdo->exec($sql);
 
-        $db->pdo-exec($sql);  
-        $sql = "ALTER TABLE `users`
-                  MODIFY `id` int NOT NULL AUTO_INCREMENT";
-        $db->pdo-exec($sql);          
+          $sql = "ALTER TABLE `users`
+            MODIFY `id` int NOT NULL AUTO_INCREMENT";
+          $db->pdo->exec($sql);
     }
 
     public function down()
     {
-        $db= \app\core\Application::$app->db;
+        $db = \app\core\Application::$app->db;
         $sql = "DROP TABLE `users`";
-        $db->pdo-exec($sql);          
+            
+        $db->pdo->exec($sql);
     }
 }
