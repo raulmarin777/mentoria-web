32682aec2ddb60884e6e5478ec36ab94715390ce
Raul Marin <raul.marin@segic.usach.cl>
2021-06-18 10:21:35 -0400

    se implementa creacion de tabla user

--- eje_framework/migrations/m00001_initial.php
+++ eje_framework/migrations/m00001_initial.php
@@ -3,12 +3,29 @@
 class m00001_initial{
     public function up()
     {
-        echo "Applying migration \n";
+        $db= \app\core\Application::$app->db;
+        $sql = "CREATE TABLE `users` (
+                                      `id` int NOT NULL,
+                               `full_name` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+                                   `email` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+                               `user_name` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+                                `password` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL
+                                      ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci"
+
+        $db->pdo-exec($sql);
+        $sql = "ALTER TABLE `users`
+                  ADD PRIMARY KEY (`id`)";
+
+        $db->pdo-exec($sql);  
+        $sql = "ALTER TABLE `users`
+                  MODIFY `id` int NOT NULL AUTO_INCREMENT";
+        $db->pdo-exec($sql);          
     }
 
     public function down()
     {
-        echo "Reversing migration";
+        $db= \app\core\Application::$app->db;
+        $sql = "DROP TABLE `users`";
+        $db->pdo-exec($sql);          
     }
-
 }
