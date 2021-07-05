3937882e0b722e654653cb576ffbc64d280c2849
Raul Marin <raul.marin@segic.usach.cl>
2021-07-05 19:07:10 -0400

    creacion api

---
+++ eje_javascript_3/api.php
@@ -0,0 +1,17 @@
+<?php
+
+    
+    $dbname = "registro";
+    $dbuser = "registro-user";
+    $dbpassword = "user1";
+    $dsn = "mysql:host=localhost;dbname=$dbname";
+        
+    $pdo = new \PDO($dsn, $dbuser, $dbpassword);
+
+    $sql ="SELECT * FROM users";
+    $statement = $this->pdo->prepare($sql);
+    $statement->execute();
+
+    $rows = $statement->fetchAll(\PDO::FETCH_ASSOC);
+
+    echo json_encode;
