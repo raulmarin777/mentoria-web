8c74a6cd227421c2af8e53043025973e73457140
Raul Marin <raul.marin@segic.usach.cl>
2021-09-15 15:42:03 -0300

    se descomenta seeders

--- eje_laravel_1/blog/database/seeders/DatabaseSeeder.php
+++ eje_laravel_1/blog/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        //\App\Models\User::factory(10)->create();
+        \App\Models\User::factory(1)->create();
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+        Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+        Category::create([
+            'name' => 'Hobbies',
+            'slug' => 'hobbies'
+        ]);
     }
 }
