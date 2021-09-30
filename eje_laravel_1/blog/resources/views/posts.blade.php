82770f1f133bf61d1577baf76691338acc47bfdc
Raul Marin <raul.marin@segic.usach.cl>
2021-09-29 23:32:52 -0300

    Se agregan estilo de post

--- eje_laravel_1/blog/resources/views/posts.blade.php
+++ eje_laravel_1/blog/resources/views/posts.blade.php
@@ -74,8 +74,8 @@
 
         @if (count($posts) > 0)
             @foreach ($posts as $post)
-
-            class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
+                <article  
+                    class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
                     <div class="py-6 px-5">
                         <div>
                             <img src="./images/illustration-3.png" alt="Blog Post illustration" class="rounded-xl">
