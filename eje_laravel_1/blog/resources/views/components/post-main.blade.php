d4158b82e04a15cef67f24f4ea2aa7433f08e2fd
Raul Marin <raul.marin@segic.usach.cl>
2021-10-01 16:42:27 -0300

    Se agrega nueva estructura slot

--- eje_laravel_1/blog/resources/views/components/post-main.blade.php
+++ eje_laravel_1/blog/resources/views/components/post-main.blade.php
@@ -1,6 +1,5 @@
 @props(['post'])
-<article
-    class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
+<article class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
     <div class="py-6 px-5 lg:flex">
         <div class="flex-1 lg:mr-8">
             <img src="/images/illustration-1.png" alt="Blog Post illustration" class="rounded-xl">
@@ -9,8 +8,8 @@
         <div class="flex-1 flex flex-col justify-between">
             <header class="mt-8 lg:mt-0">
                 <div class="space-x-2">
-                    <a href="#">
-                        $post->title
+                    <a href="#" class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"  style="font-size: 10px">
+                       {{ $post->category->name }}
                     </a>
                 </div>
 
@@ -20,7 +19,7 @@
                     </h1>
 
                     <span class="mt-2 block text-gray-400 text-xs">
-                            Published <time> {{ $post->created_ad->diffForHumans() }} </time>
+                            Published <time> {{ $post->created_at->diffForHumans() }} </time>
                         </span>
                 </div>
             </header>
@@ -35,7 +34,7 @@
                 <div class="flex items-center text-sm">
                     <img src="/images/lary-avatar.svg" alt="Lary avatar">
                     <div class="ml-3">
-                        <h5 class="font-bold">Lary Laracore</h5>
+                        <h5 class="font-bold">{{ $post->author->name }}</h5>
                         <h6>Mascot at SEGIC</h6>
                     </div>
                 </div>
