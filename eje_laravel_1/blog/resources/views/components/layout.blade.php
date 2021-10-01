ab78f48206a2151cbf7ff485ed094ef8f99251bc
Raul Marin <raul.marin@segic.usach.cl>
2021-10-01 17:15:14 -0300

    Se agrega nueva estructura slot

--- eje_laravel_1/blog/resources/views/components/layout.blade.php
+++ eje_laravel_1/blog/resources/views/components/layout.blade.php
@@ -45,7 +45,7 @@
                         <button type="submit"
                                 class="transition-colors duration-300 bg-blue-500 hover:bg-blue-600 mt-4 lg:mt-0 lg:ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-8"
                         >
-                            Subscribe
+                            Subscribe 
                         </button>
                     </form>
                 </div>
--- eje_laravel_1/blog/resources/views/components/post-card.blade.php
+++ eje_laravel_1/blog/resources/views/components/post-card.blade.php
@@ -35,7 +35,7 @@
                 </p>
 
                 <p class="mt-4">
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Duis aute irure dolor  in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                 </p>
             </div>
 
