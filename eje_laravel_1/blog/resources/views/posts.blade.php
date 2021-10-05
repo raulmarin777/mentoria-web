c7465be33bf38921eb03770a674532013d246696
Raul Marin <raul.marin@segic.usach.cl>
2021-10-05 15:32:57 -0300

    prueba merge

--- eje_laravel_1/blog/resources/views/posts.blade.php
+++ eje_laravel_1/blog/resources/views/posts.blade.php
@@ -6,8 +6,7 @@
 
         <div class="lg:grid lg:grid-cols-6">
             @foreach ($posts->skip(1) as $post)
-                @dd($loop)
-                <x-post-card  :post="$post" class="bla"/>
+                <x-post-card  :post="$post" class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2' }}"/>
             @endforeach
 
         </div>
