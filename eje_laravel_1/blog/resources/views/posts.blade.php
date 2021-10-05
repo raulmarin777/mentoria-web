be3826268f84342d4f22a5f70461ab0e8cb8c47d
Raul Marin <raul.marin@segic.usach.cl>
2021-10-05 15:24:44 -0300

    prueba merge

--- eje_laravel_1/blog/resources/views/posts.blade.php
+++ eje_laravel_1/blog/resources/views/posts.blade.php
@@ -4,15 +4,11 @@
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         <x-post-main :post="$posts[0]" />
 
-        <div class="lg:grid lg:grid-cols-2">
-            <x-post-card />
-            <x-post-card />
-        </div>
-
-        <div class="lg:grid lg:grid-cols-3">
+        <div class="lg:grid lg:grid-cols-6">
             @foreach ($posts->skip(1) as $post)
-                <x-post-card />
-            @endfor
+                <x-post-card  :post="$post" class="bg-red-500"/>
+            @endforeach
+
         </div>
     </main>
 </x-layout>
