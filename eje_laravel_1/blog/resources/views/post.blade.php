6ca873afff5c689b824fb30953ac4a3ee59f80ba
Raul Marin <raul.marin@segic.usach.cl>
2021-10-05 21:03:34 -0300

    creacion de Category-button

--- eje_laravel_1/blog/resources/views/post.blade.php
+++ eje_laravel_1/blog/resources/views/post.blade.php
@@ -36,7 +36,7 @@
                         </a>
 
                         <div class="space-x-2">
-                            <a href="#"
+                            <a href="/category/{category:id}"
                                 class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
                                 style="font-size: 10px">{{ $post->category->name }}</a>
                         </div>
