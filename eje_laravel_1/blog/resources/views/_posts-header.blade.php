354a5978b346929c51924014276b1604aa8eea06
Raul Marin <raul.marin@segic.usach.cl>
2021-10-07 16:04:25 -0300

    se incluye alpine

--- eje_laravel_1/blog/resources/views/_posts-header.blade.php
+++ eje_laravel_1/blog/resources/views/_posts-header.blade.php
@@ -15,7 +15,7 @@
         <!--  Category -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
             <div x-data="{ show: false}">
-                <button @click="alert('hola')">Category</button>
+                <button @click="show = !show">Category</button>
                 <a href="#" x-show="show">One</a>
                 <a href="#" x-show="show">Two</a>
                 <a href="#" x-show="show">Three</a>
