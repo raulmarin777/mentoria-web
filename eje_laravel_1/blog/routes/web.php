04c0bd61f37f0194946e0f3546b04a0df15d25c9
Raul Marin <raul.marin@segic.usach.cl>
2021-10-07 11:43:46 -0300

    creacion de Category-button

--- eje_laravel_1/blog/routes/web.php
+++ eje_laravel_1/blog/routes/web.php
@@ -46,7 +46,7 @@ Route::get('/category/{category:slug}', function (Category $category) {
     return view ('categorys', [
         /*'posts' => $category->posts->load(['category','author']),*/
         'posts' =>  Post::join('categories','categories.id','=','posts.category_id')
-                    ->where('categories.id', $category)
+                    ->where('posts.category_id', '1')
                     ->latest('published_at')
                     ->with(['category','author'])
                     ->get()
