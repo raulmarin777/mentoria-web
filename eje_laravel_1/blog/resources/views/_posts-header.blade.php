a0cb9b7fed0a5950a5a090780a128fbfe90855f7
Raul Marin <raul.marin@segic.usach.cl>
2021-10-22 15:23:55 -0300

    se aplica estilo

--- eje_laravel_1/blog/resources/views/_posts-header.blade.php
+++ eje_laravel_1/blog/resources/views/_posts-header.blade.php
@@ -14,8 +14,9 @@
     <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
         <!--  Category -->
         <div class="relative lg:inline-flex items-center bg-gray-100 rounded-xl">
-            <div x-data="{ show: false }" @click.away="show = false">
-                <button @click="show = !show" class="py-2 pl-3 pr-9 text-sm font-semibold lg:w-32 w-full flex lg:inline-flex">
+            <x-dropdown>
+                <x-slot name='trigger'>
+                <button class="py-2 pl-3 pr-9 text-sm font-semibold lg:w-32 w-full flex lg:inline-flex">
                     {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
 
                     <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
@@ -28,18 +29,20 @@
                         </g>
                     </svg>
                 </button>
-                <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl z-50 text-left px-3 text-sm leading-6" style="display: none;">
-                    @foreach ($categories as $category)
-                        <a href="/category/{{ $category->slug }}" 
-                        class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 hover:text-white
-                            {{ isset($currentCategory) && $currentCategory->is($category) ? 'bg-blue-500 text-white' : '' }}">
-                            {{ ucwords($category->name) }} 
-                        </a>    
-                    @endforeach
-                </div>
+                </x-slot>
+                
+                <a href="/" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 hover:text-white">
+                    All
+                </a>
 
-            </div>
-
+                @foreach ($categories as $category)
+                    <a href="/category/{{ $category->slug }}" 
+                    class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 hover:text-white
+                        {{ isset($currentCategory) && $currentCategory->is($category) ? 'bg-blue-500 text-white' : '' }}">
+                        {{ ucwords($category->name) }} 
+                    </a>    
+                @endforeach
+            </x-dropdown>
         </div>
 
         <!-- Other Filters -->
