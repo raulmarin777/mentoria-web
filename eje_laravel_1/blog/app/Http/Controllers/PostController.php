c0170025071e1601abdb06d61ea816e026f4b0b5
Raul Marin <raul.marin@segic.usach.cl>
2021-11-09 15:43:48 -0300

    correccion scope

--- eje_laravel_1/blog/app/Http/Controllers/PostController.php
+++ eje_laravel_1/blog/app/Http/Controllers/PostController.php
@@ -19,7 +19,7 @@ class PostController extends Controller
 
         return view('posts', [
             'posts' => Post::latest('published_at')
-                        ->filter(request(['search']))
+                        ->filter(request(['search', 'category']))
                         ->get(),
             'categories' => Category::all(),
         ]);
