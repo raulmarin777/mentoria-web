84884306f182bd0aa1762c3a570919b716d067f1
Raul Marin <raul.marin@segic.usach.cl>
2021-11-09 15:41:26 -0300

    correccion scope

--- eje_laravel_1/blog/app/Models/Post.php
+++ eje_laravel_1/blog/app/Models/Post.php
@@ -24,14 +24,42 @@ class Post extends Model
           $query->when(
             $filters['search'] ?? false,
             fn($query, $search) =>
-            $query->where('title','like',"%$search%")
-                  ->orWhere('resumen','like',"%$search%")
+                $query->where('title','like',"%$search%")
+                      ->orWhere('resumen','like',"%$search%")
         );
+
+       /* return $query->when(
+            $filters['category'] ?? false,
+            fn($query, $category) =>
+                $query->whereExists(function ($query){
+                $query->from ('categories')
+                      ->whereColumn('categoies.id','posts.category_id')
+                      ->where('categories.slug', $category)
+                })
+        );*/
+
 
         /*if ($filters['search'] ?? false) {
             return $query->where('title','like','%' . $filters['search'] . '%' )
                         ->orWhere('resumen','like','%' . $filters['search'] . '%' );
         }*/
+
+        $query->when(
+            $filters['category'] ?? false,
+            fn($query, $category) =>
+               $query->whereHas('category', fn($query)=>
+                                               $query->where('slug', $category))
+        );
+/* metodo antiguo
+        return $query->when(
+            $filters['category'] ?? false,
+            function($query, $category){
+                $query->whereHas('category', function($query) use ($category){
+                    $query->where('slug', $category);
+                }
+            }
+        );*/
+        return $query;
     }
 
     //hasOne, hasMany, belongsTo, belongsToMany
