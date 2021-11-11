366fc618f874d555c459ee57bf9f8f3cf35f9943
Raul Marin <raul.marin@segic.usach.cl>
2021-11-11 15:07:45 -0300

    exploracion de collection

--- eje_laravel_1/blog/app/Http/Controllers/PostController.php
+++ eje_laravel_1/blog/app/Http/Controllers/PostController.php
@@ -17,14 +17,19 @@ class PostController extends Controller
                 ->orWhere('resumen','like','%' . request('search') . '%' );
         }*/
 
-        return view('posts', [
+
+        return Post::latest('published_at')
+                        ->filter(request(['search', 'category']))
+                        ->get();
+
+        /*return view('posts', [
             'posts' => Post::latest('published_at')
                         ->filter(request(['search', 'category']))
                         ->get(),
             'categories' => Category::all(),
             'currentCategory' =>
                             request('category') != null ? Category::where('slug', request('category'))->first() : null
-        ]);
+        ]);*/
     }
 
     public function show(Post $post){
